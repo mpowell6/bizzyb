0889a7e4e2e378d8e2147dc93264139f8b526997
Mitchell Powell <powell.mitchell.d@gmail.com>
2021-03-22 21:46:55 -0400

    adding resources/home

--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -2,15 +2,20 @@ import React from "react";
 import HomeModal from "./HomeModal";
 import "./Home.css";
 
+const blog = require('../../resources/home.json');
+
 const HomeCard = () => {
   return (
     <div className="BlogCard">
+      {
+        console.log(blog)
+      }
       <div className="card text-center">
         <div className="overflow">Card Picture</div>
         <div className="card-body text-dark">
-          <h4 className="card-title">Card Title</h4>
-          <p className="card-text text-secondary">Body</p>
-          <HomeModal />
+          <h4 className="card-title">{blog[0].title}</h4>
+          <p className="card-text text-secondary">{blog[0].body}</p>
+          <HomeModal title={blog[0].title} pic={blog[0].title} body={blog[0].body}/>
         </div>
       </div>
     </div>
