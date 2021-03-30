a14ce46d745d510e6f568428fd972a83c41a3856
Mitchell <powell.mitchell.d@gmail.com>
2021-03-29 21:58:19 -0400

    Deleted extra imgs/Applied text/styling to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./Footer.css";
 import { Row, Col } from "react-bootstrap";
 
+const footerInfo = require('../../resources/footer.json');
+
 const Footer = () => {
   return (
     <div className="main-footer">
-      <div className="container">
-        <Row>
-          <Col>
-            <p>Some footer nonsense.</p>
+        <Row className="footerContainer">
+          <Col className="footerInfo">
+            <h4>{footerInfo.homeImprovement}</h4>
+            <p>{footerInfo.homeImpStatement}</p>
+          </Col>
+          <Col className="footerInfo">
+            <h4>{footerInfo.homeRemodeling}</h4>
+            <p>{footerInfo.remodelStatement}</p>
+          </Col>
+          <Col className="footerInfo">
+            <h4>{footerInfo.knoxvilleContractors}</h4>
+            <p>{footerInfo.knoxContractorsStatement}</p>
+          </Col>
+          <Col className="footerInfo">
+            <h4>{footerInfo.knoxvillePlumbing}</h4>
+            <p>{footerInfo.knoxPlumbingStatement}</p>
           </Col>
         </Row>
-      </div>
     </div>
   );
 };
